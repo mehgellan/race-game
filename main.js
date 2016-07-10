1c00e770e52714b3c453b68a1c3c3a11833cee39
mehgellan <baumegg@gmail.com>
2016-07-10 16:21:36 -0700

    set up race countdown

--- main.js
+++ main.js
@@ -5,40 +5,74 @@ $(document).on('ready', function() {
     winnerIs: '',
     jake: 0,
     finn: 0,
-    audio: new Audio('img/flylo-glitc.mp3')
+    audio: new Audio('img/flylo-glitc.mp3'),
   };
 
-
-  toggleReset();
   startGame();
 
-  $(document).on('keypress', function handleKeyPress(event) {
-    // animates box left
-    if (event.which==100) {
-      $('#finn').animate({left: "+=50"}, "fast");
+  function startGame() {
+    $('.start').click(function() {
+      countDown();
+    });
+    stopAudio();
+    handleKeyPress();
+  }
+
+  function countDown() {
+    var count = 3;
+    var counter = setInterval(timer, 1000);
+    function timer() {
+      count--;
+      if (count > 0) {
+        $('.countdown').html(count);
+      } else {
+        $('.countdown').html("GO!");
+        clearInterval(counter);
+        playAudio();
+        $('.countdown').parent().fadeOut(500);
+      }
     }
-    if (event.which==107) {
-      $('#jake').animate({left: "+=50"}, "fast");
-    }
-    detectWinner();
-  });
+  }
+
+  function handleKeyPress() {
+    $(document).on('keypress', function(event) {
+      // animates box left
+      if (event.which==100) {
+        $('#finn').animate({left: "+=50"}, "fast");
+      }
+      if (event.which==107) {
+        $('#jake').animate({left: "+=50"}, "fast");
+      }
+      detectWinner();
+    });
+  }
 
   function detectWinner() {
-    var rowWidth = $('.row').outerWidth();
-    var boxOneOffset = $('#finn').offset().left;
-    var boxTwoOffset = $('#jake').offset().left;
+    var rowWidth = $('.row').outerWidth(),
+        boxOneOffset = $('#finn').offset().left,
+        boxTwoOffset = $('#jake').offset().left
+    ;
     if (boxOneOffset >= rowWidth) {
       game.winnerIs = 'finn';
       $('.box').clearQueue().stop(true);
-      game.finn++;
+      // game.finn++;
       console.log(game.finn);
+      // game.keyOff();
     } else if (boxTwoOffset >= rowWidth) {
       game.winnerIs = 'jake';
       $('.box').clearQueue().stop(true);
-      game.jake++;
+      // game.jake++;
     }
     toggleWinnerBanner();
     toggleReset();
+  }
+
+  function toggleWinnerBanner() {
+    if (game.winnerIs==='finn') {
+      $('.target').text('Finn!');
+    } else if (game.winnerIs==='jake') {
+      $('.target').text('Jake!');
+    }
   }
 
   function toggleReset() {
@@ -48,21 +82,18 @@ $(document).on('ready', function() {
       $('.reset').hide();
     }
     $('.reset').on('click', function(event) {
-      event.preventDefault();
+      if (game.winnerIs==='finn') {
+        game.finn++;
+      } else if (game.winnerIs==='jake') {
+        game.jake++;
+      }
+      // event.preventDefault();
       $('.box').css({left:0});
       $('.target').text('');
       game.winnerIs = '';
       $('.reset').hide();
       console.log(game.finn, game.jake, game.winnerIs);
     });
-  }
-
-  function startGame() {
-    $('.start').click(function() {
-      $(this).parent().fadeOut(500);
-    });
-    playAudio();
-    stopAudio();
   }
 
   function playAudio() {
@@ -77,11 +108,4 @@ $(document).on('ready', function() {
     });
   }
 
-  function toggleWinnerBanner() {
-    if (game.winnerIs==='finn') {
-      $('.target').text('Finn!');
-    } else if (game.winnerIs==='jake') {
-      $('.target').text('Jake!');
-    }
-  }
 });
