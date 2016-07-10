2f0501f1aac1ca6de0a5dd578850b9ada9793bb7
mehgellan <baumegg@gmail.com>
2016-07-09 20:27:09 -0700

    toggle styling

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 $(document).on('ready', function() {
 
+  console.log("JS linked");
+  // $('button').hide();
   $('button').on('click', function(event) {
     event.preventDefault();
     $('.box').css({left:0});
     console.log("button clicked");
   });
-
-  console.log("JS linked");
   $(document).on('keypress', function handleKeyPress(event) {
     // animates box left
     if (event.which==100) {
@@ -16,19 +16,21 @@ $(document).on('ready', function() {
       $('#jake').animate({left: "+=100"}, "fast");
     }
     detectWinner();
-    // alerting the winner
-
 
   });
 
   function detectWinner() {
-    var rowWidth = $('.row').outerWidth();
-    if ($('#finn').offset().left >= rowWidth) {
+    var rowWidth = $('.row').outerWidth;
+    if (($('#finn').offset().left && $('#jake').offset().left) === rowWidth) {
+      $('.target').text("It's a tie!");
+    } else if ($('#finn').offset().left >= rowWidth) {
       $('.box').clearQueue().stop(true);
       $('.target').text("Finn!");
+      $('.button').show();
     } else if ($('#jake').offset().left >= rowWidth) {
       $('.box').clearQueue().stop(true);
       $('.target').text("Jake!");
+      $('.button').show();
     }
   }
 
